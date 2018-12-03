e7d0cd1ccc16d8bed4292787b454fb5fcc41c6b4
NickEast12 <nickeast1998@gmail.com>
2018-12-03 15:04:33 +0000

    commit

--- main-page.js
+++ main-page.js
@@ -1,13 +1,19 @@
 const profileBtn = document.getElementById('open-profile');
 const closeProfile = document.getElementById('profile-close');
 const profile = document.getElementById('profile-menu');
+const testT = document.getElementById('homepage-test');
+const testM = document.getElementById('homepage-body');
+
 
 
 profileBtn.addEventListener('click', () => {
     profile.style.transform = "translateX(0)";
-
+    testT.style.display = 'block';
+    testM.style.position = 'fixed';
 })
 closeProfile.addEventListener('click', () => {
     profile.style.transform = "translateX(-100%)";
+    testT.style.display = 'none';
+    testM.style.position = 'inherit';
 
 })
