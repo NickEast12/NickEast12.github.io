711931d000dc509389bca334696bc33160111822
NickEast12 <nickeast1998@gmail.com>
2018-12-03 22:34:44 +0000

    commit

--- main-page.js
+++ main-page.js
@@ -3,6 +3,12 @@ const closeProfile = document.getElementById('profile-close');
 const profile = document.getElementById('profile-menu');
 const testT = document.getElementById('homepage-test');
 const testM = document.getElementById('homepage-body');
+const colorMeRed = document.querySelector('.heart');
+const colorMeRed1 = document.getElementById('heart1');
+const colorMeRed2 = document.getElementById('heart2');
+const colorMeRed3 = document.getElementById('heart3');
+
+console.log(colorMeRed);
 
 
 
@@ -15,5 +21,38 @@ closeProfile.addEventListener('click', () => {
     profile.style.transform = "translateX(-100%)";
     testT.style.display = 'none';
     testM.style.position = 'inherit';
-
 })
+
+
+
+colorMeRed.addEventListener('click', () => {
+    if (colorMeRed.style.color === 'white') {
+        colorMeRed.style.color = 'red';
+    }else {
+        colorMeRed.style.color = 'white';
+    }
+})
+colorMeRed1.addEventListener('click', () => {
+    if (colorMeRed1.style.color === 'white') {
+        colorMeRed1.style.color = 'red';
+    }else {
+        colorMeRed1.style.color = 'white';
+    }
+})
+colorMeRed2.addEventListener('click', () => {
+    if (colorMeRed2.style.color === 'white') {
+        colorMeRed2.style.color = 'red';
+    }else {
+        colorMeRed2.style.color = 'white';
+    }
+})
+colorMeRed3.addEventListener('click', () => {
+    if (colorMeRed3.style.color === 'white') {
+        colorMeRed3.style.color = 'red';
+    }else {
+        colorMeRed3.style.color = 'white';
+    }
+})
+
+
+
