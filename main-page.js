7795680c2756a573309a952ee11215037120e188
NickEast12 <nickeast1998@gmail.com>
2018-12-17 10:47:05 +0000

    commit

--- main-page.js
+++ main-page.js
@@ -1,28 +1,8 @@
-const profileBtn = document.getElementById('open-profile');
-const closeProfile = document.getElementById('profile-close');
-const profile = document.getElementById('profile-menu');
-const testT = document.getElementById('homepage-test');
-const testM = document.getElementById('homepage-body');
+
 const colorMeRed = document.querySelector('.heart');
 const colorMeRed1 = document.getElementById('heart1');
 const colorMeRed2 = document.getElementById('heart2');
 const colorMeRed3 = document.getElementById('heart3');
-
-console.log(colorMeRed);
-
-
-
-profileBtn.addEventListener('click', () => {
-    profile.style.transform = "translateX(0)";
-    testT.style.display = 'block';
-    testM.style.position = 'fixed';
-})
-closeProfile.addEventListener('click', () => {
-    profile.style.transform = "translateX(-100%)";
-    testT.style.display = 'none';
-    testM.style.position = 'inherit';
-})
-
 
 
 colorMeRed.addEventListener('click', () => {
