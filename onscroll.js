c01be26d55788229830bfc8080799b95f11b52fe
NickEast12 <nickeast1998@gmail.com>
2019-01-31 21:56:40 +0000

    Desktop

---
+++ onscroll.js
@@ -0,0 +1,30 @@
+let width = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+let height = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+    
+    console.log(width);
+    if (width = 1440) {
+        alert('this is' + width)
+        function Scroll () {
+            let top = document.getElementById('navbar');
+            let ypos = window.pageYOffset;
+                if (ypos > 350) {
+                    top.style.height = '700px'
+                    top.style.backgroundColor = 'red';
+                }else{
+                    top.style.height = '150px';
+                    top.style.backgroundColor = 'inherit';
+                }
+                
+        }
+        window.addEventListener('scroll', Scroll); 
+    }else {
+        console.log ('nattt')
+    }
+    
+    
+
