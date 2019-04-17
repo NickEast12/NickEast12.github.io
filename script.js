60fc5641ab60bf8b415393e4c91671452d3ef134
NickEast12 <nickeast1998@gmail.com>
2019-04-17 16:34:44 +0100

    commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // make it pull back the menu
 
 let hamburgerAgain = document.querySelector('.hamburger');
-let nav = document.querySelector('.navigation');
+let nav = document.querySelector('.mobile-overlay');
 let upArrow = document.querySelector('.fas');
 hamburgerAgain.addEventListener('click', () => {
     hamburger.classList.toggle("is-active"); 
@@ -12,6 +12,6 @@ hamburgerAgain.addEventListener('click', () => {
     hamburger.classList.toggle("is-active"); 
 });
 upArrow.addEventListener('click', () =>{
-    nav.style.transform = 'translateY(-120%)';
+    nav.style.transform = 'translateY(-100%)';
     hamburger.classList.toggle("is-active");
 });
